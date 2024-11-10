dbc745d2f59d021490c21ee35c2fa1a1af59a874
KamaraIlham <kamarailham10@gmail.com>
2024-11-10 23:02:34 +0600

    simplesample

--- new.js
+++ new.js
@@ -1,2 +1,5 @@
-let random = (a,b) => a+b;
-console.log(random(5,6))
+//let random = (a,b) => a+b;
+//console.log(random(5,6))
+
+// document.getElementById('title').innerHTML = 'Hello World' ;
+document.querySelector('#title').innerHTML = 'Hey world' ;
