aa0eac46ba41de78b3f41354c35d84eea60287e6
KamaraIlham <kamarailham10@gmail.com>
2024-11-15 20:38:50 +0600

    Updated

--- new.js
+++ new.js
@@ -2,4 +2,25 @@
 //console.log(random(5,6))
 
 // document.getElementById('title').innerHTML = 'Hello World' ;
-document.querySelector('#title').innerHTML = 'Hey world' ;
+// document.querySelector('#title').innerHTML = 'Hey world' ;
+
+
+document.getElementById('btn').addEventListener('click' , () => {
+    const numberDice = document.getElementById('numberDice').value
+    const result = document.getElementById('result')
+    const images = document.getElementById('images')
+    const values = []
+    const imagess = []
+
+    for(let i = 0; i < numberDice; i++) {
+        const value = Math.floor (Math.random() * 6) + 1
+        console.log(value)
+        imagess.push(`<img src="img/${value}.png ` )
+
+
+    }
+
+    result.textContent = ` dice == ${values.join(', ')}`
+    images.innerHTML = imagess.join(' ')
+
+})
