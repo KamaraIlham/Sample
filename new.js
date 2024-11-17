9dc92fc99398a55bbbd97d6ca28ed0c119399a63
KamaraIlham <kamarailham10@gmail.com>
2024-11-17 21:40:56 +0600

    Update new.js

--- new.js
+++ new.js
@@ -1,8 +1,3 @@
-//let random = (a,b) => a+b;
-//console.log(random(5,6))
-
-// document.getElementById('title').innerHTML = 'Hello World' ;
-// document.querySelector('#title').innerHTML = 'Hey world' ;
 
 
 document.getElementById('btn').addEventListener('click' , () => {
